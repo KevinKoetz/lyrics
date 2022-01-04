5c3c8f2d9474c4fe9ebd77b2e827502d16f5a1b4
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-01-04 18:02:47 +0100

    Lyrics App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+class App extends React.Component {
+  state = {
+    artist: null,
+    title: null,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Lyrics.ovh</h1>
+        <p>Only the Lyrics</p>
+        <SearchBar
+          onSongSelect={(artist, title) => this.setState({ artist, title })}
+        />
+        {this.state.artist && this.state.title ? (
+          <Lyrics artist={this.state.artist} title={this.state.title} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const createDebouncer = () => {
+  let timeout = null;
+  return (callback) => {
+    if(timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      callback()
+      clearTimeout(timeout);
+  }, 500)
+  }
+}
+
+const debounce = createDebouncer()
+
+function SearchBar({ onSongSelect }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(()=>debounce(async ()=>{
+    if(!searchInput) return
+    const url = `https://api.lyrics.ovh/suggest/${searchInput}`;
+    const response = await axios.get(url);
+    const firstTen = response.data.data.slice(0, 10);
+    setSuggestions(firstTen);
+  }), [searchInput])
+
+
+  const handleSongSelect = (artist, title) => {
+    onSongSelect(artist, title);
+    setSearchInput("");
+    setSuggestions([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <input
+          type="text"
+          placeholder="Type the song you want the lyrics"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
+      <ul className="suggestions">
+        {suggestions.map((song) => (
+          <li
+            key={song.id}
+            onClick={() => handleSongSelect(song.artist.name, song.title)}
+          >
+            {song.title} - {song.artist.name}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Lyrics({ artist, title }) {
+  const [lyrics, setLyrics] = useState("");
+
+  useEffect(() => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    axios.get(url).then((response) => setLyrics(response.data.lyrics));
+  }, [artist, title]);
+  return (
+    <section>
+      <h2>{artist} - {title}</h2>
+      <p>{lyrics}</p>
+    </section>
   );
 }
 
